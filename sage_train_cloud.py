9cbaacdc6f5ddcd5d7226c67ae835d640928f5dd
cvrooman <vroomang@gmail.com>
2019-11-09 17:13:53 -0700

    updating cloud params

--- sage_train_cloud.py
+++ sage_train_cloud.py
@@ -20,7 +20,6 @@ estimator = Estimator(role=role,
 # The fit function uploads the tar.gz file to the bucket.
 # fit checks to see if this file is actually there, but it may not be using it.
 estimator.fit('s3://<insert bucket path to csv>')
-# estimator.fit('file:///tmp/cifar-10-data')
 # estimator.fit('file://./data/pima-indians-diabetes.csv')
 
 # Code - https://github.com/awslabs/amazon-sagemaker-examples/blob/master/advanced_functionality/tensorflow_bring_your_own/tensorflow_bring_your_own.ipynb
